d8190fcca760aad63831d8ae4d75342bc496d287
Tshiamo Bhuda <tshiamobhuda@gmail.com>
2020-04-12 18:19:35 +0200

    Format long lines

--- init.py
+++ init.py
@@ -11,8 +11,11 @@ from vpn_config import IMG_DIR
 APP_NAME = 'FortiVPN-Quick-Tray'
 
 if __name__ == "__main__":
-    indicator = AppIndicator3.Indicator.new(APP_NAME, IMG_DIR + '/off.png',
-                                            AppIndicator3.IndicatorCategory.SYSTEM_SERVICES)
+    indicator = AppIndicator3.Indicator.new(
+        APP_NAME,
+        IMG_DIR + '/off.png',
+        AppIndicator3.IndicatorCategory.SYSTEM_SERVICES
+    )
     indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
 
     builder = MenuBuilder(Gtk, indicator)
