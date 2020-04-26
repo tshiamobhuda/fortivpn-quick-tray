2376f04cc7381500be575dfdc301b50fc1c798a7
Tshiamo Bhuda <tshiamobhuda@gmail.com>
2020-04-26 05:34:47 +0200

    Re-write app into one file - Added Indicator class -  initialize Indicator through - added mock action method

--- init.py
+++ init.py
@@ -8,19 +8,61 @@ from gi.repository import Gtk, AppIndicator3
 from menu_items import MenuBuilder
 from os import path
 
+class Indicator():
+    def __init__(self):
+        self.indicator = AppIndicator3.Indicator.new_with_path(
+            'FortiVPN-Quick-Tray',
+            'off',
+            AppIndicator3.IndicatorCategory.APPLICATION_STATUS,
+            path.dirname(path.realpath(__file__)) + '/icons'
+        )
+        self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
+        self.indicator.set_label('FortiVPN OFF', 'FortiVPN OFF')
+        self.indicator.set_menu(self._build_menu())
+
+    def _build_menu(self):
+        menu = Gtk.Menu()
+
+        self.connect_menu_item = Gtk.MenuItem('Connect')
+        self.disconnect_menu_item = Gtk.MenuItem('Disonnect')
+        self.config_menu_item = Gtk.MenuItem('Config')
+        self.logs_menu_item = Gtk.MenuItem('Logs')
+        self.exit_menu_item = Gtk.MenuItem('Exit')
+
+        self.connect_menu_item.connect('activate', self._click_exit)
+        self.disconnect_menu_item.connect('activate', self._click_disconnect)
+        self.config_menu_item.connect('activate', self._click_config)
+        self.logs_menu_item.connect('activate', self._click_logs)
+        self.exit_menu_item.connect('activate', self._click_exit)
+        
+        menu.append(self.connect_menu_item)
+        menu.append(self.disconnect_menu_item)
+        menu.append(self.config_menu_item)
+        menu.append(self.logs_menu_item)
+        menu.append(self.exit_menu_item)
+
+        menu.show_all()
+
+        return menu
+
+    def _click_connect(self, object):
+        pass
+
+    def _click_disconnect(self, object):
+        pass
+
+    def _click_logs(self, object):
+        pass
+
+    def _click_config(self, object):
+        pass
+
+    def _click_exit(self, object):
+        Gtk.main_quit()
+
 if __name__ == "__main__":
-    indicator = AppIndicator3.Indicator.new_with_path(
-        'FortiVPN-Quick-Tray',
-        'off',
-        AppIndicator3.IndicatorCategory.APPLICATION_STATUS,
-        path.dirname(path.realpath(__file__)) + '/icons'
-    )
 
-    indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
-    indicator.set_label('FortiVPN OFF', 'FortiVPN OFF')
-
-    builder = MenuBuilder(Gtk, indicator, AppIndicator3)
-    indicator.set_menu(builder.build_menu())
+    Indicator()
 
     signal.signal(signal.SIGINT, signal.SIG_DFL)
     Gtk.main()
